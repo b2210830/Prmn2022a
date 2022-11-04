a69a3024831373edacfdf1ab053febe245da3e99
b2210830 <b2210830@photon.chitose.ac.jp>
2022-11-04 16:57:57 +0900

    課題1-3

---
+++ prmn2022a/src/lecture01/lecture1_3.java
@@ -0,0 +1,18 @@
+package lecture01;
+
+import  java.util.Scanner;
+
+public class lecture1_3 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int age = input.nextInt();
+
+        if (age < 20) {
+            System.out.println("I am " + age + " years old so I cannot drink liquor.");
+        } else {
+            System.out.println("I am " + age + " years old so I can drink liquor.");
+        }
+    }
+}
