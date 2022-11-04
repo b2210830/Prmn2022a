229ae6c65d70019f077723d9c15f4ffb7db39a3b
b2210830 <b2210830@photon.chitose.ac.jp>
2022-11-04 15:57:56 +0900

    課題1-2

---
+++ prmn2022a/src/lecture01/lecture1_2.java
@@ -0,0 +1,10 @@
+package lecture01;
+
+public class lecture1_2 {
+
+    public static void main(String[] args){
+
+        int studentNumber = 2210830;
+        System.out.println("B" + studentNumber + " Riri Kimura");
+    }
+}
