8f1a3f52da4ce8ce79df0d62593b49d6a7b546b6
b2210830 <b2210830@photon.chitose.ac.jp>
2022-11-08 21:47:12 +0900

    課題2-2

---
+++ prmn2022a/src/lecture02/Car.java
@@ -0,0 +1,20 @@
+package lecture02;
+
+public class Car {
+    int fuel;
+
+    Car(){
+        this.fuel = 0;
+    }
+
+    void run(){
+        if(fuel <=0 ){
+            System.out.println("燃料がないので走れませんでした。");
+        }else{
+            System.out.println("燃料を1消費して走りました。");
+        }
+    }
+
+
+
+}
