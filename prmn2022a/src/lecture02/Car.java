85f0653c7201425b2ff4d6eb29093b46d8f3564d
b2210830 <b2210830@photon.chitose.ac.jp>
2022-11-08 23:20:38 +0900

    課題2-3

--- prmn2022a/src/lecture02/Car.java
+++ prmn2022a/src/lecture02/Car.java
@@ -2,10 +2,16 @@ package lecture02;
 
 public class Car {
     int fuel;
+    Tire[] tires;
+    Engine engines;
 
-    Car(){
+    Car(Tire[] tires, Engine engines){
         this.fuel = 0;
+        this.tires = tires;
+        this.engines = engines;
+
     }
+
 
     void run(){
         if(fuel <=0 ){
@@ -13,6 +19,11 @@ public class Car {
         }else{
             System.out.println("燃料を1消費して走りました。");
         }
+    }
+
+    void startEngine(){
+        Engine engine = new Engine(engines.rpm);
+        engine.start();
     }
 
 
--- prmn2022a/src/lecture02/Exercise2_3.java
+++ prmn2022a/src/lecture02/Exercise2_3.java
@@ -1,16 +1,18 @@
 package lecture02;
 
-public class Exercise2_2 {
+public class Exercise2_3 {
     public static void main(String[] args) {
         Tire[] tire = new Tire[4];
         Engine engine = new Engine(4000);
 
         Car car = new Car((Tire[]) tire,(Engine) engine);
-        GasStation gasStation = new GasStation();
 
-        car.run();
+        GasStation gasStation = new GasStation();
         gasStation.refuel((Car) car);
+
+        car.startEngine();
         car.run();
+
 
     }
 }
