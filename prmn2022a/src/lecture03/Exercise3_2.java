489cdeafc639e87d102aaca676ec31b51f2996b3
b2210830 <b2210830@photon.chitose.ac.jp>
2022-11-15 23:32:21 +0900

    課題3-2

---
+++ prmn2022a/src/lecture03/Exercise3_2.java
@@ -0,0 +1,31 @@
+package lecture03;
+
+import java.util.Scanner;
+
+public class Exercise3_2 {
+
+    public static void main(String[] args) {
+
+        System.out.println("何行分入力しますか？");
+
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+
+        String[] ArrayList = new String[n];
+
+        for (int i = 0; i < ArrayList.length; i++) {
+            System.out.println((i + 1) + "行目：");
+            ArrayList[i] = input.next();
+        }
+
+        System.out.println("入力した文字列：");
+
+        int num = 0;
+
+        for (String arrayList : ArrayList) {
+            System.out.println("["+num+"]"+arrayList);
+            num++;
+        }
+
+    }
+}
