f1b7fb14e91a16bca8714a9c85115828f46d4b10
b2210830 <b2210830@photon.chitose.ac.jp>
2022-11-30 09:11:51 +0900

    課題5-2

--- prmn2022a/src/lecture05/Insect.java
+++ prmn2022a/src/lecture05/Insect.java
@@ -1,9 +1,8 @@
 package lecture05;
 
 public class Insect{
-
-        public void move () {
-            System.out.println("歩いたよ");
-        }
+    public void move () {
+        System.out.println("歩いたよ");
     }
+}
 
